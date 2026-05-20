2443083834be1fe8467eec66735dd0f48c1ff8ed
leedlqhdk <leedlqhdk@gmail.com>
2026-05-20 20:14:09 +0900

    Update majang.ts

--- client/src/data/areas/majang.ts
+++ client/src/data/areas/majang.ts
@@ -2,37 +2,37 @@ export const majangPosts = [
   {
     title: "마장면 원룸 복도 정기관리",
     date: "2026.05.20",
-    image: "/images/areas/majang-1.jpg",
+    image: "/images/areas/majang/majang-1.jpg",
   },
 
   {
     title: "마장면 빌라 계단청소",
     date: "2026.05.18",
-    image: "/images/areas/majang-2.jpg",
+    image: "/images/areas/majang/majang-2.jpg",
   },
 
   {
     title: "마장면 공동현관 계단 관리",
     date: "2026.05.15",
-    image: "/images/areas/majang-3.jpg",
+    image: "/images/areas/majang/majang-3.jpg",
   },
 
   {
     title: "마장면 공용복도 정기청소",
     date: "2026.05.12",
-    image: "/images/areas/majang-4.jpg",
+    image: "/images/areas/majang/majang-4.jpg",
   },
 
   {
     title: "마장면 창틀 및 유리 관리",
     date: "2026.05.10",
-    image: "/images/areas/majang-5.jpg",
+    image: "/images/areas/majang/majang-5.jpg",
   },
 
   {
     title: "마장면 계단 광택관리",
     date: "2026.05.08",
-    image: "/images/areas/majang-6.jpg",
+    image: "/images/areas/majang/majang-6.jpg",
   },
 ];
 export const majangReviews = [
