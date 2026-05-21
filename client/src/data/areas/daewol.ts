8ae6912c385327269d71d3fd8280152ac5862da8
leedlqhdk <leedlqhdk@gmail.com>
2026-05-21 18:04:02 +0900

    Update daewol.ts

--- client/src/data/areas/daewol.ts
+++ client/src/data/areas/daewol.ts
@@ -2,32 +2,32 @@ export const daewolPosts = [
   {
     title: "대월면 빌라 계단청소",
     date: "2026.05.20",
-    image: "/images/areas/daewol/daewol-1",
+    image: "/images/areas/daewol/daewol-1.jpg",
   },
   {
     title: "대월면 창틀 먼지 제거",
     date: "2026.05.18",
-    image: "/images/areas/daewol/daewol-2",
+    image: "/images/areas/daewol/daewol-2.jpg",
   },
   {
     title: "대월면 공동 현관 유리관리",
     date: "2026.05.15",
-    image: "/images/areas/daewol/daewol-3",
+    image: "/images/areas/daewol/daewol-3.jpg",
   },
   {
     title: "대월면 계단 바닥 정기관리",
     date: "2026.05.02",
-    image: "/images/areas/daewol/daewol-4",
+    image: "/images/areas/daewol/daewol-4.jpg",
   },
    {
     title: "대월면 빌라 계단 정기관리",
     date: "2026.04.19",
-    image: "/images/areas/daewol/daewol-5",
+    image: "/images/areas/daewol/daewol-5.jpg",
   },
    {
     title: "대월면 빌라 거미줄 제거",
     date: "2026.04.10",
-    image: "/images/areas/daewol/daewol-6",
+    image: "/images/areas/daewol/daewol-6.jpg",
   },
 ];
 
