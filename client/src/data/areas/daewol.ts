eaae7916e826a43dcc806c8d3983984812721b60
leedlqhdk <leedlqhdk@gmail.com>
2026-05-21 20:49:21 +0900

    Fix Daewol area image paths

--- client/src/data/areas/daewol.ts
+++ client/src/data/areas/daewol.ts
@@ -2,7 +2,7 @@ export const daewolPosts = [
   {
     title: "대월면 빌라 계단청소",
     date: "2026.05.20",
-    image: "/images/areas/daewol/daewol-1.jpg",
+    image: "/images/areas/daewol/daewol-1.webp",
   },
   {
     title: "대월면 창틀 먼지 제거",
@@ -12,22 +12,12 @@ export const daewolPosts = [
   {
     title: "대월면 공동 현관 유리관리",
     date: "2026.05.15",
-    image: "/images/areas/daewol/daewol-3.jpg",
+    image: "/images/areas/daewol/daewol-3.webp",
   },
   {
-    title: "대월면 계단 바닥 정기관리",
-    date: "2026.05.02",
-    image: "/images/areas/daewol/daewol-4.jpg",
-  },
-   {
     title: "대월면 빌라 계단 정기관리",
     date: "2026.04.19",
-    image: "/images/areas/daewol/daewol-5.jpg",
-  },
-   {
-    title: "대월면 빌라 거미줄 제거",
-    date: "2026.04.10",
-    image: "/images/areas/daewol/daewol-6.jpg",
+    image: "/images/areas/daewol/daewol-5.webp",
   },
 ];
 
