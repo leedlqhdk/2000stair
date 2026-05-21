ed26d69dd0f24b6948792f39deeb1b888ebe2b50
leedlqhdk <leedlqhdk@gmail.com>
2026-05-21 13:04:30 +0900

    Update BeforeAfterGallery.tsx

--- client/src/components/BeforeAfterGallery.tsx
+++ client/src/components/BeforeAfterGallery.tsx
@@ -6,8 +6,8 @@ const galleryItems = [
   {
     id: 1,
     title: "빌라 계단 바닥 오염",
-    before: "/images/stair before_1.jpg",
-    after: "/images/stair after_1.jpg",
+    before: "/images/stair-before1.jpg",
+    after: "/images/stair-after1.jpg",
   },
   {
     id: 2,
