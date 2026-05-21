09e4f8ebbddad565acc3586a2ed10903d42b0f48
leedlqhdk <leedlqhdk@gmail.com>
2026-05-21 19:22:00 +0900

    fix: smooth scroll to homepage sections from navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,6 +23,23 @@ function ScrollToTop() {
   const [location] = useLocation();
 
   useEffect(() => {
+    const hash = window.location.hash;
+
+    if (hash) {
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+
+        if (element) {
+          element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      }, 120);
+
+      return;
+    }
+
     window.scrollTo({ top: 0, left: 0, behavior: "auto" });
   }, [location]);
 
