bea7e22dd608d20309520777b053ec044da2621c
leedlqhdk <leedlqhdk@gmail.com>
2026-05-22 12:00:45 +0900

    Update majang.ts

--- client/src/data/areas/majang.ts
+++ client/src/data/areas/majang.ts
@@ -1,24 +1,24 @@
 export const majangPosts = [
   {
-    title: "마장면 원룸 복도 정기관리",
+    title: "양촌사거리 원룸건물 복도 정기관리",
     date: "2026.05.20",
     image: "/images/areas/majang/majang-1.webp",
   },
 
   {
-    title: "마장면 빌라 계단청소",
+    title: "오천교 삼거리 빌라 계단청소",
     date: "2026.05.18",
     image: "/images/areas/majang/majang-2.webp",
   },
 
   {
-    title: "마장면 공동현관 계단 관리",
+    title: "오천리 공동현관 계단 관리",
     date: "2026.05.15",
     image: "/images/areas/majang/majang-3.webp",
   },
 
   {
-    title: "마장면 공용복도 정기청소",
+    title: "덕평로 빌라 공용복도 정기청소",
     date: "2026.05.12",
     image: "/images/areas/majang/majang-5.webp",
   },
