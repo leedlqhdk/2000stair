2e5f438fa372c0037760e19a84cd5992f642e3f5
leedlqhdk <leedlqhdk@gmail.com>
2026-06-07 00:28:10 +0900

    Point Vercel trpc function to appRouter

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,5 +1,5 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../server/routers";
+import { appRouter } from "../server/appRouter";
 import { createContext } from "../server/_core/context";
 import express from "express";
 
