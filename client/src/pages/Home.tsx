ea85f2e6f8d3ac78242bf5a1e4b40dfc64e58361
leedlqhdk <leedlqhdk@gmail.com>
2026-06-07 16:48:34 +0900

    Add HusbandProfileStats component to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <HowItWorks />
         <BeforeAfterGallery />
         <PricingSection isAuthenticated={false} />
+        <HusbandProfileStats />
       </main>
     </div>
   );
