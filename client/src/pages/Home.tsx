194b9c61d2773fc7722ffef41bf0df6873db8bdd
leedlqhdk <leedlqhdk@gmail.com>
2026-06-22 15:01:10 +0900

    Refactor Home component layout and comments

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,37 +10,37 @@ import FaqSection from "@/components/FaqSection";
 import QuoteForm from "@/components/QuoteForm";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col bg-background">
-      <Navbar />
+return (
+<div className="min-h-screen flex flex-col bg-background">
+<Navbar />
 
-      <main className="flex-1">
-        <HeroSection isAuthenticated={false} />
+<main className="flex-1">
+<HeroSection isAuthenticated={false} />
 
-        {/* 제공 서비스 */}
-        <Services />
+{/* 제공 서비스 */}
+<Services />
 
-        {/* 왜 이천계단지기인가 */}
-        <HowItWorks />
+{/* 왜 이천계단지기인가 */}
+<HowItWorks />
 
-        {/* 대표 직접관리 */}
-        <HusbandProfileStats />
+{/* 대표 직접관리 */}
+<HusbandProfileStats />
 
-        {/* 실제 작업 결과 */}
-        <BeforeAfterGallery />
+{/* 실제 작업 결과 */}
+<BeforeAfterGallery />
 
-        {/* 정기관리 요금 안내 */}
-        <PricingOverview />
+{/* 실제 후기 요약 */}
+<div className="container max-w-6xl py-16 md:py-24">
+<BlogReviews />
+</div>
 
-        <QuoteForm />
+{/* 정기관리 요금 안내 */}
+<PricingOverview />
 
-        {/* 실제 후기 요약 */}
-        <div className="container max-w-6xl py-16 md:py-24">
-          <BlogReviews />
-        </div>
+<FaqSection />
 
-        <FaqSection />
-      </main>
-    </div>
-  );
+<QuoteForm />
+</main>
+</div>
+);
 }
