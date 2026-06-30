c9cab623e8394a550bbaad3f14a5c0ab9b25499c
leedlqhdk <leedlqhdk@gmail.com>
2026-06-30 17:04:28 +0900

    Fix rotated downtown work photos

--- client/src/data/areas/downtown.ts
+++ client/src/data/areas/downtown.ts
@@ -2,9 +2,9 @@ import type { AreaPost } from "@/hooks/useAreaPosts";
 
 export const downtownPosts: AreaPost[] = [
   { title: "중리동 빌라 계단청소", date: "2026.05.20", image: "/images/areas/downtown/downtown-1.webp", area: "jungni" },
-  { title: "관고동 상가건물 관리", date: "2026.05.18", image: "/images/areas/downtown/downtown-2.webp", area: "gwango" },
-  { title: "관고동 상가 계단 정기청소", date: "2026.05.15", image: "/images/areas/downtown/downtown-3.webp", area: "gwango" },
+  { title: "관고동 상가건물 관리", date: "2026.05.18", image: "/images/areas/downtown/downtown-2-fixed.jpg", area: "gwango" },
+  { title: "관고동 상가 계단 정기청소", date: "2026.05.15", image: "/images/areas/downtown/downtown-3-fixed.jpg", area: "gwango" },
   { title: "중리동 빌라 계단 바닥 정기관리", date: "2026.05.12", image: "/images/areas/downtown/downtown-4.webp", area: "jungni" },
-  { title: "창전동 연립빌라 공동현관 유리코팅", date: "2026.04.19", image: "/images/areas/downtown/downtown-5.webp", area: "changjeon" },
-  { title: "안흥동 빌라 정기관리", date: "2026.04.10", image: "/images/areas/downtown/downtown-6.webp", area: "jeungpo" },
+  { title: "창전동 연립빌라 공동현관 유리코팅", date: "2026.04.19", image: "/images/areas/downtown/downtown-5-fixed.jpg", area: "changjeon" },
+  { title: "안흥동 빌라 정기관리", date: "2026.04.10", image: "/images/areas/downtown/downtown-6-fixed.jpg", area: "jeungpo" },
 ];
