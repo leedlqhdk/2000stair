e9ee3fc3ea1ddd0558d50aefef3af48389ccf171
leedlqhdk <leedlqhdk@gmail.com>
2026-08-14 19:07:29 +0900

    Place process section before services

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -50,11 +50,11 @@ export default function Home() {
                 {/* 대표 직접관리 */}
                 <HusbandProfileStats />
 
-                {/* 제공 서비스 */}
-                <Services />
-
                 {/* 왜 이천계단지기인가 */}
                 <HowItWorks />
+
+                {/* 제공 서비스 */}
+                <Services />
 
                 {/* 실제 작업 결과 */}
                 <BeforeAfterGallery />
